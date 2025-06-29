dae2814c779c422f76532afa5dd089fdb31c4be5
rudramangukiya123 <rudramangukiya28@gmail.com>
2025-06-29 18:44:51 +0200

    schema updated

--- app/models/mongo_schemas.py
+++ app/models/mongo_schemas.py
@@ -10,11 +10,6 @@ class FamilyModel(BaseModel):
     email: EmailStr
     password: PasswordStr
     fullName: StrippedStr
-    age: Optional[int]
-    gender: Optional[str]
-    dietaryPreferences: Optional[str]
-    allergies: List[str] = []
-    prakriti: Optional[str]
     isVerified: bool = False
     createdAt: Optional[datetime] = Field(default_factory=datetime.utcnow)
     updatedAt: Optional[datetime] = Field(default_factory=datetime.utcnow)
